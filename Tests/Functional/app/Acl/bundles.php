01ef7849dce826fa190910b73aa49e13e266ec8c
Iltar van der Berg <ivanderberg@hostnet.nl>
2016-04-19 12:17:42 +0200

    Load the doctrine bundle as well

--- Tests/Functional/app/Acl/bundles.php
+++ Tests/Functional/app/Acl/bundles.php
@@ -10,6 +10,7 @@
  */
 
 return array(
+    new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
     new Symfony\Bundle\AclBundle\AclBundle(),
     new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
     new Symfony\Bundle\AclBundle\Tests\Functional\Bundle\TestBundle\TestBundle(),
